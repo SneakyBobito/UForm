a41a146685769909125eb80ce20b2881c85e740d
sghzal <sghzal@gmail.com>
2015-09-07 12:11:35 +0200

    Length validator

---
+++ lib/UForm/Validation/Validator/StringLength.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace UForm\Validation\Validator;
+
+use \UForm\Validation\Validator,
+	\UForm\Validation\Exception,
+	\UForm\Validation;
+
+
+class StringLength extends Validator{
+
+    private $_minLength;
+    private $_maxLength;
+
+    function __construct($minLength = 0, $maxLength = 0, $messages = [])
+    {
+        $this->_minLength = $minLength;
+        $this->_maxLength = $maxLength;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function validate(Validation $validator){
+        
+        $value = $validator->getValue();
+
+        if($this->_minLength > 0 && strlen($value) < $this->_minLength){
+            $validator->appendMessage(
+                $this->getOption('string-too-short', 'String too short (less than ' . $this->_minLength . ')')
+            );
+            return false;
+        }
+        if($this->_maxLength > 0 && strlen($value) > $this->_maxLength){
+            $validator->appendMessage(
+                $this->getOption('string-too-short', 'String too long (more than ' . $this->_maxLength . ')')
+            );
+            return false;
+        }
+
+
+        return true;
+    }
+}
