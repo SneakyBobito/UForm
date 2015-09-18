fef1dda7cf85904a73fd71053018674ef514c875
sghzal <sghzal@gmail.com>
2015-09-18 10:58:00 +0200

    DirectClosure Test

--- lib/UForm/Filter/DirectClosure.php
+++ lib/UForm/Filter/DirectClosure.php
@@ -6,15 +6,46 @@ use UForm\Filter;
 
 /**
  * Usefull to create filter on the fly by only passing a callback function
+ * the filter method will use this closure by passing the filtered value as the first parameter
+ *
+ * <code>
+ *  $directClosure = new DirectClosure(
+ *      function($v){
+ *          return 'foo' . $v;
+ *      }
+ * );
+ *
+ * var_dump($directClosure->filter("bar"));
+ *
+ * // > string(6) "foobar"
+ * </code>
+ *
  */
 class DirectClosure extends Filter {
-    
+
+    /**
+     * @var callable
+     */
     public $closure;
-    
-    function __construct($closure) {
+
+    /**
+     * @param callable $closure the closure that will be called during filtering. The closure first parameter will be the filtered value
+     */
+    function __construct(callable $closure) {
         $this->closure = $closure;
     }
-    
+
+    /**
+     * get the internal closure
+     * @return callable
+     */
+    public function getClosure(){
+        return $this->closure;
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function filter($v) {
         $closure = $this->closure;
         return $closure($v);
---
+++ test/suites/UForm/Test/Filter/DirectClosureTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @license see LICENSE
+ */
+
+namespace UForm\Test\Filter;
+
+
+use UForm\Filter\DirectClosure;
+
+class DirectClosureTest extends \PHPUnit_Framework_TestCase {
+
+    public function testDirectClosure(){
+        $closure = function($v){
+            return "-$v-";
+        };
+        $directClosure = new DirectClosure($closure);
+
+        $this->assertSame($closure, $directClosure->getClosure());
+
+        $this->assertEquals("-foo-", $directClosure->filter("foo"));
+
+    }
+}
