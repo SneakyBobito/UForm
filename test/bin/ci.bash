6170e6fc6201d5860b741b5dfd4e67106fbdd0ff
sghzal <sghzal@gmail.com>
2015-09-18 10:17:00 +0200

    Fix ci exiting with status 1

--- test/bin/ci.bash
+++ test/bin/ci.bash
@@ -7,4 +7,7 @@ SCRIPTDIR=$(dirname "$SCRIPTFILE")
 
 phpunit --debug -c "$SCRIPTDIR/../../phpunit.dist.xml" --coverage-clover "$SCRIPTDIR/../../build/logs/clover.xml" --process-isolation
 
-[ "$PROCESS_CODECLIMAE" = true ] &&  ./vendor/bin/test-reporter
+if [ "$PROCESS_CODECLIMAE" = true ]
+then
+    ./vendor/bin/test-reporter
+fi
