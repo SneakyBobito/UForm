656e8f6f2f490986a23dcdd848f7f40c74d40a96
sghzal <sghzal@gmail.com>
2015-09-18 16:54:50 +0200

    Remove obsolete bootstrap builder

--- lib/UForm/Builder.php
+++ lib/UForm/Builder.php
@@ -266,4 +266,64 @@ class Builder {
         return $this;
     }
 
+
+
+
+    /**
+     * add a left addon for input group (bootstrap inputs only)
+     * @param $text
+     * @return $this
+     * @throws \Exception
+     * @throws \UForm\Forms\Exception
+     */
+    public function leftAddon($text){
+        $this->last()->setUserOption("leftAddon", $text);
+        return $this;
+    }
+
+    /**
+     * add a right addon for input group (bootstrap inputs only)
+     * @param $text
+     * @return $this
+     * @throws \Exception
+     * @throws \UForm\Forms\Exception
+     */
+    public function rightAddon($text){
+        $this->last()->setUserOption("rightAddon", $text);
+        return $this;
+    }
+
+
+
+    /**
+     * @param null $name
+     * @param int $width
+     * @return $this
+     * @throws \Exception
+     */
+    public function column($width, $name = null)
+    {
+
+        $element = new Column($name);
+
+        $this->_add($element);
+        $this->_stack($element);
+
+        $element->addClass("col-md-$width");
+        $element->setUserOption("col-width", $width);
+
+        return $this;
+
+    }
+
+
+    public function panel($name){
+        $element = new Panel($name);
+        $this->_add($element);
+        $this->_stack($element);
+
+        return $this;
+    }
+
+
 }
