39e6cb6e3f71010ceb85b2f1960c0f17116f89b6
sghzal <sghzal@gmail.com>
2015-09-19 22:03:49 +0200

    fix test

--- test/bin/ci.bash
+++ test/bin/ci.bash
@@ -5,9 +5,7 @@ set -e
 SCRIPTFILE=$(readlink -f "$0")
 SCRIPTDIR=$(dirname "$SCRIPTFILE")
 
-#phpunit --debug -c "$SCRIPTDIR/../../phpunit.dist.xml" --coverage-clover "$SCRIPTDIR/../../build/logs/clover.xml" --process-isolation
-
-
+phpunit --debug -c "$SCRIPTDIR/../../phpunit.dist.xml" --coverage-clover "$SCRIPTDIR/../../build/logs/clover.xml" --process-isolation
 
 $SCRIPTDIR/phpcs.bash $1
 
