1ed3fd7e3ad96fe72e6766ee9bcd2e0782ca8893
sghzal <sghzal@gmail.com>
2015-09-19 21:50:48 +0200

    better tests strucutre

--- test/bin/ci.bash
+++ test/bin/ci.bash
@@ -5,9 +5,11 @@ set -e
 SCRIPTFILE=$(readlink -f "$0")
 SCRIPTDIR=$(dirname "$SCRIPTFILE")
 
-phpunit --debug -c "$SCRIPTDIR/../../phpunit.dist.xml" --coverage-clover "$SCRIPTDIR/../../build/logs/clover.xml" --process-isolation
+#phpunit --debug -c "$SCRIPTDIR/../../phpunit.dist.xml" --coverage-clover "$SCRIPTDIR/../../build/logs/clover.xml" --process-isolation
 
-./vendor/bin/phpcs --standard="./test/phpcs/ruleset.xml" --report="summary"
+
+
+$SCRIPTDIR/phpcs.bash $1
 
 if [ "$PROCESS_CODECLIMAE" = true ]
 then
