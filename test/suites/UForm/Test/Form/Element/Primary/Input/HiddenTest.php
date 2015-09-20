433dbff2671182b39493e8405dde0e6f6eb6a3a5
sghzal <sghzal@gmail.com>
2015-09-20 18:28:16 +0200

    Input Hidden tests

---
+++ test/suites/UForm/Test/Form/Element/Primary/Input/HiddenTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @license see LICENSE
+ */
+
+namespace UForm\Test\Form\Element\Primary\Input;
+
+
+use UForm\Form\Element\Primary\Input;
+use UForm\Form\Element\Primary\Input\Hidden;
+
+class HiddenTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var Input
+     */
+    protected $input;
+
+    public function setUp(){
+        $this->input = new Hidden("inputname");
+    }
+
+    public function testConstruct(){
+        $this->assertTrue($this->input->hasSemanticType("input:hidden"));
+    }
+
+    public function testRender(){
+        $render = $this->input->render(["inputname" => "inputValue"], ["inputname" => "inputValue"]);
+
+        $expected = '<input type="hidden" name="inputname" value="inputValue"/>';
+        $this->assertEquals($expected, $render);
+    }
+
+}
