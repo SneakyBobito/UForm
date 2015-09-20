f8ef8aad45bb9c08f8bdc5664af2001834a32a09
sghzal <sghzal@gmail.com>
2015-09-20 21:59:14 +0200

    psr2

--- test/suites/UForm/Test/Form/Element/Primary/TextAreaTest.php
+++ test/suites/UForm/Test/Form/Element/Primary/TextAreaTest.php
@@ -5,20 +5,21 @@
 
 namespace UForm\Test\Form\Element\Primary;
 
-
 use UForm\Form\Element\Primary\TextArea;
 
-class TextAreaTest extends \PHPUnit_Framework_TestCase {
+class TextAreaTest extends \PHPUnit_Framework_TestCase
+{
 
-    public function testConstruct(){
+    public function testConstruct()
+    {
         $textArea = new TextArea("textarea");
         $this->assertTrue($textArea->hasSemanticType("textarea"));
     }
 
-    public function testRender(){
+    public function testRender()
+    {
         $textArea = new TextArea("textarea");
         $expected = '<textarea name="textarea">some text</textarea>';
         $this->assertEquals($expected, $textArea->render(["textarea" => "some text"], ["textarea" => "some text"]));
     }
-
 }
