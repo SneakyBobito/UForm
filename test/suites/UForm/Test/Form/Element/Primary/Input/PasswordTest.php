a39d0fab8418276f32607ac4bcc4d8ddb09dac3a
sghzal <sghzal@gmail.com>
2015-09-20 18:29:05 +0200

    Input Password tests

---
+++ test/suites/UForm/Test/Form/Element/Primary/Input/PasswordTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @license see LICENSE
+ */
+
+namespace UForm\Test\Form\Element\Primary\Input;
+
+
+use UForm\Form\Element\Primary\Input;
+use UForm\Form\Element\Primary\Input\Password;
+
+class PasswordTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @var Input
+     */
+    protected $input;
+
+    public function setUp(){
+        $this->input = new Password("inputname");
+    }
+
+    public function testConstruct(){
+        $this->assertTrue($this->input->hasSemanticType("input:password"));
+    }
+
+    public function testRender(){
+        $render = $this->input->render(["inputname" => "inputValue"], ["inputname" => "inputValue"]);
+
+        $expected = '<input type="password" name="inputname" value="inputValue"/>';
+        $this->assertEquals($expected, $render);
+    }
+
+}
