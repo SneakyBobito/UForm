d4dd45a4e1e67386f284fb12aadf1e7481225e6d
sghzal <sghzal@gmail.com>
2015-09-22 10:00:10 +0200

    Abstractoption test

---
+++ test/suites/UForm/Test/Form/Element/Primary/Select/AbstractOptionTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @license see LICENSE
+ */
+
+namespace UForm\Test\Form\Element\Primary\Select;
+
+use UForm\Form\Element\Primary\Select;
+use UForm\Form\Element\Primary\Select\AbstractOption;
+
+class AbstractOptionTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var AbstractOption
+     */
+    protected $abstractOption;
+
+    public function setUp()
+    {
+        $this->abstractOption = $this->getMockForAbstractClass(
+            "UForm\Form\Element\Primary\Select\AbstractOption",
+            ["optionName"]
+        );
+    }
+
+
+    public function testGetLabel()
+    {
+        $this->assertSame("optionName", $this->abstractOption->getLabel());
+    }
+
+    public function testSetSelect()
+    {
+        $select = new Select("select");
+        $this->abstractOption->setSelect($select);
+        $this->assertSame($select, $this->abstractOption->getSelect());
+    }
+
+    public function testGetSelect()
+    {
+        $this->assertNull($this->abstractOption->getSelect());
+    }
+}
