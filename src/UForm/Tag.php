7302cb4521e8772a45a8a606ba754b357755c791
Soufiane Ghzal <sghzal@gmail.com>
2017-05-19 16:01:26 +0200

    Tag constructor properties must be array

--- src/UForm/Tag.php
+++ src/UForm/Tag.php
@@ -7,12 +7,12 @@ namespace UForm;
  */
 class Tag
 {
-    
+
     protected $name;
     protected $baseProperties;
     protected $closed;
-            
-    public function __construct($name, $baseProperties = [], $closed = false)
+
+    public function __construct($name, array $baseProperties = [], $closed = false)
     {
         $this->name = $name;
         $this->baseProperties = $baseProperties ;
@@ -31,9 +31,9 @@ class Tag
         if (null === $p) {
             $p = [];
         }
-        
+
         $class = isset($this->baseProperties['class']) ? $this->baseProperties['class'] : null;
-        
+
         if (isset($p['class'])) {
             if ($class) {
                 $class .= ' ' . $p['class'];
@@ -44,11 +44,11 @@ class Tag
 
         $properties = array_merge($this->baseProperties, $p);
 
-        
+
         if ($class) {
             $properties['class'] = $class;
         }
-        
+
         $d = '';
 
         foreach ($properties as $k => $v) {
@@ -61,21 +61,21 @@ class Tag
                 $d .= ' ' . $ks . '="' . $vs . '"';
             }
         }
-        
+
         return $d;
     }
-    
+
     public function draw($p = null, $content = '')
     {
         $d = '<' . $this->name;
         $d .= $this->__parseProperties($p);
-        
+
         if ($this->closed) {
             $d .= '/>';
         } else {
             $d .= ">$content</$this->name>";
         }
-        
+
         return $d;
     }
 }
