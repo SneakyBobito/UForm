4845c0c4ec119e49fc5e05b0fdbc9717910ae4d5
Soufiane Ghzal <sghzal@gmail.com>
2017-06-06 10:24:35 +0200

    make checkbox checkable by default

--- src/UForm/Form/Element/Primary/Input/Check.php
+++ src/UForm/Form/Element/Primary/Input/Check.php
@@ -14,22 +14,27 @@ use UForm\Form\Element\Primary\Input;
  */
 class Check extends Input
 {
-    public function __construct($name)
+
+    protected $defaultChecked;
+
+    public function __construct($name, $defaultChecked = false)
     {
         parent::__construct('checkbox', $name);
         $this->addSemanticType('input:checkbox');
+        $this->defaultChecked = $defaultChecked;
     }
 
     protected function overridesParamsBeforeRender($params, $value)
     {
-        if (isset($value[$this->getName()]) && $value[$this->getName()]) {
+        if (isset($value[$this->getName()])) {
+            if ($value[$this->getName()]) {
+                $params['checked'] = 'checked';
+            }
+        } elseif ($this->defaultChecked) {
             $params['checked'] = 'checked';
         }
+
         $params['value'] = 1;
         return $params;
-    }
-
-    public function sanitizeData()
-    {
     }
 }
--- test/suites/UForm/Test/Form/Element/Primary/Input/CheckTest.php
+++ test/suites/UForm/Test/Form/Element/Primary/Input/CheckTest.php
@@ -5,10 +5,14 @@
 
 namespace UForm\Test\Form\Element\Primary\Input;
 
+use UForm\Filter\BooleanValue;
+use UForm\Filter\BoolToInt;
+use UForm\Filter\DefaultValue;
+use UForm\Form;
 use UForm\Form\Element\Primary\Input\Check;
 
 /**
- * @covers UForm\Form\Element\Primary\Input\Check
+ * @covers \UForm\Form\Element\Primary\Input\Check
  */
 class CheckTest extends \PHPUnit_Framework_TestCase
 {
@@ -21,17 +25,34 @@ class CheckTest extends \PHPUnit_Framework_TestCase
 
     public function testRender()
     {
-        $input = new Check('inputname', 'yes');
+        $input = new Check('inputname');
         $id = $input->getId();
         $render = $input->render(['inputname' => true]);
         $expected = '<input type="checkbox" name="inputname" id="' . $id . '" value="1" checked="checked"/>';
         $this->assertEquals($expected, $render);
 
-        // no value
+        // false value
         $input = new Check('inputname');
         $id = $input->getId();
         $render = $input->render(['inputname' => false]);
         $expected = '<input type="checkbox" name="inputname" id="' . $id . '" value="1"/>';
+        $this->assertEquals($expected, $render);
+    }
+
+    public function testRenderNoValue()
+    {
+        // No value
+        $input = new Check('inputname');
+        $id = $input->getId();
+        $render = $input->render([]);
+        $expected = '<input type="checkbox" name="inputname" id="' . $id . '" value="1"/>';
+        $this->assertEquals($expected, $render);
+
+        // no value - default checked
+        $input = new Check('inputname', true);
+        $id = $input->getId();
+        $render = $input->render([]);
+        $expected = '<input type="checkbox" name="inputname" id="' . $id . '" checked="checked" value="1"/>';
         $this->assertEquals($expected, $render);
     }
 }
