6c2b79f78319cabde51e5a199f40561f2bb3d9b0
Soufiane Ghzal <sghzal@gmail.com>
2017-08-04 12:25:08 +0200

    IntegerValue min max

--- src/UForm/Validator/IntegerValue.php
+++ src/UForm/Validator/IntegerValue.php
@@ -12,6 +12,25 @@ use UForm\Validator;
 class IntegerValue extends Validator
 {
     const NOT_INTEGER = 'IntegerValue::NOT_INTEGER';
+    const TOO_LARGE   = 'IntegerValue::TOO_LARGE';
+    const TOO_SMALL   = 'IntegerValue::TOO_SMALL';
+
+    protected $min;
+    protected $max;
+
+    /**
+     * IntegerValue constructor.
+     * @param $min
+     * @param $max
+     */
+    public function __construct($min = null, $max = null, $options = [])
+    {
+        $this->min = $min;
+        $this->max = $max;
+
+        parent::__construct($options);
+    }
+
 
     public function validate(ValidationItem $validationItem)
     {
@@ -26,6 +45,26 @@ class IntegerValue extends Validator
 
             $validationItem->appendMessage($message);
             $validationItem->setInvalid();
+        } else {
+            if (null !== $this->min && $this->min > $value) {
+                $message = new Message(
+                    'Number too small (minimum is %_min_%)',
+                    ['min' => $this->min],
+                    self::NOT_INTEGER
+                );
+
+                $validationItem->appendMessage($message);
+                $validationItem->setInvalid();
+            } elseif (null !== $this->max && $this->max < $value) {
+                $message = new Message(
+                    'Number too large (maximum is %_max_%)',
+                    ['max' => $this->max],
+                    self::NOT_INTEGER
+                );
+
+                $validationItem->appendMessage($message);
+                $validationItem->setInvalid();
+            }
         }
     }
 }
--- test/suites/UForm/Test/Validator/IntergerValueTest.php
+++ test/suites/UForm/Test/Validator/IntergerValueTest.php
@@ -51,4 +51,91 @@ class IntergerValueTest extends ValidatorTestCase
         $validator->validate($validation);
         $this->assertFalse($validation->isValid());
     }
+
+    public function testMin()
+    {
+        $validator = new IntegerValue(1);
+
+        $validation = $this->generateValidationItem(['firstname' => 5]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => 1]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+
+        $validation = $this->generateValidationItem(['firstname' => -1]);
+        $validator->validate($validation);
+        $this->assertFalse($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => 0]);
+        $validator->validate($validation);
+        $this->assertFalse($validation->isValid());
+    }
+
+    public function testMax()
+    {
+        $validator = new IntegerValue(null, 5);
+
+        $validation = $this->generateValidationItem(['firstname' => 5]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => 1]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => -1]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => 0]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+
+        $validation = $this->generateValidationItem(['firstname' => 6]);
+        $validator->validate($validation);
+        $this->assertFalse($validation->isValid());
+    }
+
+    public function testMinMax()
+    {
+        $validator = new IntegerValue(2, 5);
+
+        $validation = $this->generateValidationItem(['firstname' => 5]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => 2]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => 3]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+
+
+        $validation = $this->generateValidationItem(['firstname' => 6]);
+        $validator->validate($validation);
+        $this->assertFalse($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => 1]);
+        $validator->validate($validation);
+        $this->assertFalse($validation->isValid());
+
+
+
+        $validator = new IntegerValue(2, 2);
+
+        $validation = $this->generateValidationItem(['firstname' => 2]);
+        $validator->validate($validation);
+        $this->assertTrue($validation->isValid());
+
+        $validation = $this->generateValidationItem(['firstname' => 3]);
+        $validator->validate($validation);
+        $this->assertFalse($validation->isValid());
+    }
 }
